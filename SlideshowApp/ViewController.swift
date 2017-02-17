8777021f82ad1f38ad7469ce746441a9ba2d113f
Yuto Yoshihara <yu-to.yoshi_y2@ezweb.ne.jp>
2017-02-17 09:16:50 +0900

    スライドショーアプリです

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var SlideshowImageView: UIImageView!
+    
+    let photos = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", ]
+    
+    
+    var timer: Timer!
+    var timer_sec: Float = 0.0
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let myImage : UIImage = UIImage(named: photos[0])!
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
